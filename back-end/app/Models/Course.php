928cf0062ab7e25ceaf7663204a7a7bbe75ed561
DomenicoCicero <domenico.cicero8@gmail.com>
2024-05-31 13:01:03 +0200

    implemented migration and relation Model

---
+++ back-end/app/Models/Course.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    public function activity(): BelongsTo
+    {
+        return $this->belongsTo(Activity::class);
+    }
+
+    public function slot(): BelongsTo
+    {
+        return $this->belongsTo(Slot::class);
+    }
+    
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)->withPivot('status');
+    }
+}
